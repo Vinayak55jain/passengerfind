304e539736868d5c7bf3840433b10b5e776ee357
Vinayak55jain <vinayakjainlife@gmail.com>
2026-06-16 20:27:09 +0530

    removed update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,6 @@ export default function Home() {
             <Skills />
             <Exploring />
             <Insights />
-             <SystemArchitecture />
             <Contact />
           </div>
         </main>
